13cbc6694b00503cff7e7af14577d615dd3704a2
Eric Müller <eric.mueller@lsl.de>
2017-08-17 19:01:33 +0200

    Added short hand method for hateoas response generation

--- src/Phrest/API/HateoasResponseGeneratorAwareTrait.php
+++ src/Phrest/API/HateoasResponseGeneratorAwareTrait.php
@@ -14,4 +14,14 @@ trait HateoasResponseGeneratorAwareTrait
     {
         $this->hateoasResponseGenerator = $hateoasResponseGenerator;
     }
+
+    /**
+     * Generates a JSON HATEOAS response based on the given data and annotations.
+     * @param $data
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    protected function generateHateoasResponse($data): \Psr\Http\Message\ResponseInterface
+    {
+        return $this->hateoasResponseGenerator->generate($data);
+    }
 }
